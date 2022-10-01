3183e46960a3cf53dd712a533c592342e277f0b5
‘cjw01139927 <cjw01139927@alibaba-inc.com>
2022-10-01 16:16:30 +0800

    first

--- app/src/main/java/com/chejw/hwq/MainActivity.java
+++ app/src/main/java/com/chejw/hwq/MainActivity.java
@@ -3,6 +3,7 @@ package com.chejw.hwq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("CHEJW","冗余信息");
+        Log.d("CHEJW","调试信息");
+        Log.i("CHEJW","一般消息");
+        Log.w("CHEJW","警告信息");
+        Log.e("CHEJW","错误信息");
     }
 }
